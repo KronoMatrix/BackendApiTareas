51ea993b385bb17a062d4a8dc81658021410578a
Erick Jimenez <erick-rubi@hotmail.com>
2024-07-16 19:58:13 -0600

    protección de Tareas

--- backend/routes/tareasRoutes.js
+++ backend/routes/tareasRoutes.js
@@ -1,12 +1,12 @@
-const express = require ('express')
+const express = require('express')
 const router = express.Router()
-const { getTareas, crearTareas, updateTareas, deleteTareas } = require ('../controllers/tareasControllers')
+const { getTareas, crearTareas, updateTareas, deleteTareas } = require('../controllers/tareasControllers')
+const { protect } = require('../middleware/authMiddleware')
 
+router.get('/', protect, getTareas)
+router.post('/', protect, crearTareas)
 
-router.get('/', getTareas)
-router.post('/', crearTareas)
-
-router.put('/:id', updateTareas)
-router.delete('/:id', deleteTareas)
+router.put('/:id', protect, updateTareas)
+router.delete('/:id', protect, deleteTareas)
 
 module.exports = router
